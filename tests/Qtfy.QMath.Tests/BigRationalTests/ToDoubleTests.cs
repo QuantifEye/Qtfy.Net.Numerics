f77a875997abdcaeb2f42a2a35915a3e46cd078b
David Naylor <naylor.b.david@gmail.com>
2020-11-14 16:41:16 +0200

    BigRational: add tests for rounding in double convertion

--- tests/Qtfy.QMath.Tests/BigRationalTests/ToDoubleTests.cs
+++ tests/Qtfy.QMath.Tests/BigRationalTests/ToDoubleTests.cs
@@ -5,6 +5,7 @@
 
 namespace Qtfy.QMath.Tests.BigRationalTests
 {
+    using System.Numerics;
     using NUnit.Framework;
 
     [TestOf(typeof(BigRational))]
@@ -43,6 +44,21 @@ namespace Qtfy.QMath.Tests.BigRationalTests
         {
             var actual = (double)(BigRational)expected;
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(1.0000000000000002, 4)]
+        [TestCase(1.0000000000000002, 3)]
+        [TestCase(1d, 2)]
+        [TestCase(1d, 1)]
+        [TestCase(1d, 0)]
+        [TestCase(1d, -1)]
+        [TestCase(0.99999999999999989, -2)]
+        [TestCase(0.99999999999999978, -3)]
+        [TestCase(0.99999999999999978, -4)]
+        public void CastToDoubleRounding(double expected, int shift)
+        {
+            var two54 = BigInteger.Pow(2, 54);
+            Assert.AreEqual(expected, (double)new BigRational(two54 + shift, two54));
         }
     }
 }
