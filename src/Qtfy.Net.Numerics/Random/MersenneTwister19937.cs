b41ecf8e108b65e4bb396fe402528ee6411c0871
andrebeelders <andrebeelders@outlook.com>
2020-12-04 15:47:30 -0500

    fixed style cop issues

--- src/Qtfy.Net.Numerics/Random/MersenneTwister19937.cs
+++ src/Qtfy.Net.Numerics/Random/MersenneTwister19937.cs
@@ -5,8 +5,12 @@
 
 namespace Qtfy.Net.Numerics.Random
 {
-    using System.Runtime.CompilerServices;
-
+    /// <summary>
+    /// The Mersenne Twister random number generator.
+    /// <see href="http://stackoverflow.com">
+    /// http://www.math.sci.hiroshima-u.ac.jp/m-mat/MT/emt.html
+    /// </see>.
+    /// </summary>
     public sealed class MersenneTwister19937 : IRandomBitGenerator<uint>
     {
         private const int N = 624;
@@ -23,21 +27,37 @@ namespace Qtfy.Net.Numerics.Random
 
         private int index;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MersenneTwister19937"/> class.
+        /// </summary>
+        /// <param name="state">
+        /// The state array of the generator.
+        /// </param>
+        /// <param name="index">
+        /// The current position in the state.
+        /// </param>
         private MersenneTwister19937(uint[] state, int index)
         {
             this.state = state;
             this.index = index;
         }
 
-        private static unsafe void InitGenRandImpl(uint* mt, uint s)
-        {
-            mt[0] = s;
-            for (uint mti = 1; mti < N; mti++)
-            {
-                mt[mti] = (1812433253U * (mt[mti - 1] ^ (mt[mti - 1] >> 30))) + mti;
-            }
-        }
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MersenneTwister19937"/> class.
+        /// </summary>
+        /// <param name="seed">
+        /// The seed used to seed the initial state.
+        /// </param>
+        /// <returns>
+        /// A new instance of a Mersenne Twister PRNG.
+        /// </returns>
+        /// <remarks>
+        /// This method uses the initialization procedure called init_by_array in the original
+        /// c code.
+        /// <see href="http://stackoverflow.com">
+        /// http://www.math.sci.hiroshima-u.ac.jp/m-mat/MT/MT2002/CODES/mt19937ar.c
+        /// </see>.
+        /// </remarks>
         public static MersenneTwister19937 InitGenRand(uint seed)
         {
             var state = new uint[N];
@@ -52,43 +72,22 @@ namespace Qtfy.Net.Numerics.Random
             return new MersenneTwister19937(state, N);
         }
 
-        private static unsafe void InitByArrayImpl(uint* mt, uint* init_key, uint key_length)
-        {
-            InitGenRandImpl(mt, 19650218U);
-            var i = 1U;
-            var j = 0U;
-            var k = key_length > N ? key_length : N;
-            for (; k != 0; k--)
-            {
-                // non linear
-                mt[i] = (mt[i] ^ ((mt[i - 1] ^ (mt[i - 1] >> 30)) * 1664525U)) + init_key[j] + j;
-                if (++i >= N)
-                {
-                    mt[0] = mt[N - 1];
-                    i = 1;
-                }
-
-                if (++j >= key_length)
-                {
-                    j = 0;
-                }
-            }
-
-            for (k = N - 1; k != 0; k--)
-            {
-                // non linear
-                mt[i] = (mt[i] ^ ((mt[i - 1] ^ (mt[i - 1] >> 30)) * 1566083941U)) - i;
-                if (++i >= N)
-                {
-                    mt[0] = mt[N - 1];
-                    i = 1;
-                }
-            }
-
-            // MSB is 1; assuring non-zero initial array
-            mt[0] = 0x80000000U;
-        }
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MersenneTwister19937"/> class.
+        /// </summary>
+        /// <param name="seeds">
+        /// The seeds used to seed the initial state.
+        /// </param>
+        /// <returns>
+        /// A new instance of a Mersenne Twister PRNG.
+        /// </returns>
+        /// <remarks>
+        /// This method uses the initialization procedure called init_by_array in the original
+        /// c code.
+        /// <see href="http://stackoverflow.com">
+        /// http://www.math.sci.hiroshima-u.ac.jp/m-mat/MT/MT2002/CODES/mt19937ar.c
+        /// </see>.
+        /// </remarks>
         public static MersenneTwister19937 InitByArray(uint[] seeds)
         {
             var state = new uint[N];
@@ -104,7 +103,23 @@ namespace Qtfy.Net.Numerics.Random
             return new MersenneTwister19937(state, N);
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveOptimization)]
+        /// <inheritdoc />
+        public uint Next()
+        {
+            if (this.index == N)
+            {
+                this.UpdateState(this.state);
+                this.index = 0;
+            }
+
+            var y = this.state[this.index++];
+            y ^= y >> 11;
+            y ^= (y << 7) & 0x9d2c5680;
+            y ^= (y << 15) & 0xefc60000;
+            y ^= y >> 18;
+            return y;
+        }
+
         private static unsafe void UpdateStateImpl(uint* mt)
         {
             var p0 = mt;
@@ -139,6 +154,49 @@ namespace Qtfy.Net.Numerics.Random
             *p0 = *p2 ^ (y >> 1) ^ ((y & 0x1U) == 0U ? 0x0U : MatrixA);
         }
 
+        private static unsafe void InitByArrayImpl(uint* mt, uint* initKey, uint keyLength)
+        {
+            InitGenRandImpl(mt, 19650218U);
+            var i = 1U;
+            var j = 0U;
+            var k = keyLength > N ? keyLength : N;
+            for (; k != 0; k--)
+            {
+                mt[i] = (mt[i] ^ ((mt[i - 1] ^ (mt[i - 1] >> 30)) * 1664525U)) + initKey[j] + j;
+                if (++i >= N)
+                {
+                    mt[0] = mt[N - 1];
+                    i = 1;
+                }
+
+                if (++j >= keyLength)
+                {
+                    j = 0;
+                }
+            }
+
+            for (k = N - 1; k != 0; k--)
+            {
+                mt[i] = (mt[i] ^ ((mt[i - 1] ^ (mt[i - 1] >> 30)) * 1566083941U)) - i;
+                if (++i >= N)
+                {
+                    mt[0] = mt[N - 1];
+                    i = 1;
+                }
+            }
+
+            mt[0] = 0x1U << 31;
+        }
+
+        private static unsafe void InitGenRandImpl(uint* mt, uint s)
+        {
+            mt[0] = s;
+            for (uint mti = 1; mti < N; mti++)
+            {
+                mt[mti] = (1812433253U * (mt[mti - 1] ^ (mt[mti - 1] >> 30))) + mti;
+            }
+        }
+
         private void UpdateState(uint[] state)
         {
             unsafe
@@ -150,22 +208,6 @@ namespace Qtfy.Net.Numerics.Random
             }
 
             this.index = 0;
-        }
-
-        public uint Next()
-        {
-            if (this.index == N)
-            {
-                UpdateState(this.state);
-                this.index = 0;
-            }
-
-            var y = this.state[this.index++];
-            y ^= y >> 11;
-            y ^= (y << 7) & 0x9d2c5680;
-            y ^= (y << 15) & 0xefc60000;
-            y ^= y >> 18;
-            return y;
         }
     }
 }
