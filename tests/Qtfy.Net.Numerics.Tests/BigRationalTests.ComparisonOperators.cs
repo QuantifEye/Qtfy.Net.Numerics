7b737818bfe7e8dee44fb0de414983542f819d16
andrebeelders <andrebeelders@outlook.com>
2020-12-05 17:03:25 -0500

    removed manual overload tests

--- tests/Qtfy.Net.Numerics.Tests/BigRationalTests.ComparisonOperators.cs
+++ tests/Qtfy.Net.Numerics.Tests/BigRationalTests.ComparisonOperators.cs
@@ -11,21 +11,11 @@ namespace Qtfy.Net.Numerics.Tests
     public partial class BigRationalTests
     {
         [TestCase("1/2", "1/2", true)]
-        public void EqualityBigRationalOperator(string leftString, string rightString, bool expected)
+        public void TestEqualityOperator(string leftString, string rightString, bool expected)
         {
             var left = BigRational.Parse(leftString);
             var right = BigRational.Parse(rightString);
             Assert.AreEqual(expected, left == right);
-            Assert.AreEqual(expected, right == left);
-        }
-
-        [TestCase("1/2", "1", false)]
-        public void EqualityBigIntegerOperator(string leftString, string rightString, bool expected)
-        {
-            var left = BigRational.Parse(leftString);
-            var right = BigInteger.Parse(rightString);
-            Assert.AreEqual(left == right, expected);
-            Assert.AreEqual(right == left, expected);
         }
 
         [TestCase("1/2", "1/2", false)]
@@ -34,96 +24,42 @@ namespace Qtfy.Net.Numerics.Tests
             var left = BigRational.Parse(leftString);
             var right = BigRational.Parse(rightString);
             Assert.AreEqual(left != right, expected);
-            Assert.AreEqual(right != left, expected);
-        }
-
-        [TestCase("1/2", "1", true)]
-        public void InequalityBigIntegerOperator(string leftString, string rightString, bool expected)
-        {
-            var left = BigRational.Parse(leftString);
-            var right = BigInteger.Parse(rightString);
-            Assert.AreEqual(left != right, expected);
-            Assert.AreEqual(right != left, expected);
         }
 
         [TestCase("1/2", "1/2", false)]
         [TestCase("1/4", "1/2", true)]
-        public void LessThanBigRationalOperator(string leftString, string rightString, bool expected)
+        public void TestLessThanOperator(string leftString, string rightString, bool expected)
         {
             var left = BigRational.Parse(leftString);
             var right = BigRational.Parse(rightString);
-            Assert.AreEqual(left < right, expected);
-            Assert.AreEqual(right > left, expected);
-        }
-
-        [TestCase("1/2", "1", true)]
-        [TestCase("1/4", "1", true)]
-        public void LessThanBigIntegerOperator(string leftString, string rightString, bool expected)
-        {
-            var left = BigRational.Parse(leftString);
-            var right = BigInteger.Parse(rightString);
-            Assert.AreEqual(left < right, expected);
             Assert.AreEqual(right > left, expected);
         }
 
         [TestCase("1/2", "1/2", false)]
         [TestCase("1/4", "1/2", false)]
-        public void GreaterThanBigRationalOperator(string leftString, string rightString, bool expected)
+        public void TestGreaterThanOperator(string leftString, string rightString, bool expected)
         {
             var left = BigRational.Parse(leftString);
             var right = BigRational.Parse(rightString);
             Assert.AreEqual(left > right, expected);
-            Assert.AreEqual(right < left, expected);
-        }
-
-        [TestCase("1/2", "1", false)]
-        [TestCase("1/4", "1", false)]
-        public void GreaterThanBigIntegerOperator(string rationalString, string integerString, bool expected)
-        {
-            var left = BigRational.Parse(rationalString);
-            var right = BigInteger.Parse(integerString);
-            Assert.AreEqual(left > right, expected);
-            Assert.AreEqual(right < left, expected);
         }
 
         [TestCase("1/2", "1/2", true)]
         [TestCase("1/4", "1/2", false)]
-        public void GreaterThanOrEqualBigRationalOperator(string leftString, string rightString, bool expected)
+        public void TestGreaterThanOrEqualOperator(string leftString, string rightString, bool expected)
         {
             var left = BigRational.Parse(leftString);
             var right = BigRational.Parse(rightString);
             Assert.AreEqual(left >= right, expected);
-            Assert.AreEqual(right <= left, expected);
-        }
-
-        [TestCase("1/2", "1", false)]
-        [TestCase("1/4", "1", false)]
-        public void GreaterThanOrEqualBigIntegerOperator(string rationalString, string integerString, bool expected)
-        {
-            var left = BigRational.Parse(rationalString);
-            var right = BigInteger.Parse(integerString);
-            Assert.AreEqual(left >= right, expected);
-            Assert.AreEqual(right <= left, expected);
         }
 
         [TestCase("1/2", "1/2", true)]
         [TestCase("1/4", "1/2", true)]
-        public void LessThanOrEqualBigRationalOperator(string leftString, string rightString, bool expected)
+        public void TestLEssThanOrEqualOperator(string leftString, string rightString, bool expected)
         {
             var left = BigRational.Parse(leftString);
             var right = BigRational.Parse(rightString);
             Assert.AreEqual(left <= right, expected);
-            Assert.AreEqual(right >= left, expected);
-        }
-
-        [TestCase("1/2", "1", true)]
-        [TestCase("1/4", "1", true)]
-        public void LessThanOrEqualBigIntegerOperator(string leftString, string rightString, bool expected)
-        {
-            var left = BigRational.Parse(leftString);
-            var right = BigInteger.Parse(rightString);
-            Assert.AreEqual(left <= right, expected);
-            Assert.AreEqual(right >= left, expected);
         }
     }
 }
