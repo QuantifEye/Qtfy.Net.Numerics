0a42158d205681fa2db2f9f443584a49e1bdff67
andrebeelders <andrebeelders@outlook.com>
2020-12-06 00:20:56 -0500

    minor style issues

--- tests/Qtfy.Net.Numerics.Tests/BigRationalTests.DecimalConversion.cs
+++ tests/Qtfy.Net.Numerics.Tests/BigRationalTests.DecimalConversion.cs
@@ -13,33 +13,33 @@ namespace Qtfy.Net.Numerics.Tests
         [TestCase("0.0125", "1/80")]
         [TestCase("-0.125", "-1/8")]
         [TestCase("-0.0125", "-1/80")]
-        public void DecimalToRational(string decimalString, string expectedString)
+        public void DecimalToRational(string dec, string expected)
         {
             AssertEqual(
-                BigRational.Parse(expectedString),
-                (BigRational)decimal.Parse(decimalString));
+                BigRational.Parse(expected),
+                (BigRational)decimal.Parse(dec));
         }
 
         [TestCase("1/8", "0.125")]
         [TestCase("1/80", "0.0125")]
         [TestCase("-1/8", "-0.125")]
         [TestCase("-1/80", "-0.0125")]
-        public void RationalToDecimalExact(string rationalString, string decimalString)
+        public void RationalToDecimalExact(string rational, string expected)
         {
-            var rational = BigRational.Parse(rationalString);
-            var expected = decimal.Parse(decimalString);
-            AssertEqual(expected, (decimal)rational);
+            AssertEqual(
+                decimal.Parse(expected),
+                (decimal)BigRational.Parse(rational));
         }
 
         [TestCase("2/3", "0.6666666666666666666666666667")]
         [TestCase("-2/3", "-0.6666666666666666666666666667")]
         [TestCase("1/3", "0.3333333333333333333333333333")]
         [TestCase("-1/3", "-0.3333333333333333333333333333")]
-        public void RationalToDecimalWithRecurringDigit(string rationalString, string expectedString)
+        public void RationalToDecimalWithRecurringDigit(string rational, string expected)
         {
-            var rational = BigRational.Parse(rationalString);
-            var expected = decimal.Parse(expectedString);
-            Assert.AreEqual(expected, (decimal)rational);
+            Assert.AreEqual(
+                decimal.Parse(expected),
+                (decimal)BigRational.Parse(rational));
         }
 
         [TestCase("0.0000000000000000000000000005", "0")]
@@ -48,12 +48,13 @@ namespace Qtfy.Net.Numerics.Tests
         [TestCase("-0.0000000000000000000000000005", "0")]
         [TestCase("-0.0000000000000000000000000015", "-0.0000000000000000000000000002")]
         [TestCase("-0.0000000000000000000000000025", "-0.0000000000000000000000000002")]
-        public void RationalToDecimalWithRounding(string leftString, string rightString)
+        public void RationalToDecimalWithRounding(string rationalAsDecimal, string expected)
         {
-            var rational = (BigRational)decimal.Parse(leftString);
-            var actual = (decimal)(rational / 10);
-            var expected = decimal.Parse(rightString);
-            Assert.AreEqual(expected, actual);
+            var r = (BigRational)decimal.Parse(rationalAsDecimal);
+            var actual = (decimal)(r / 10);
+            Assert.AreEqual(
+                decimal.Parse(expected),
+                actual);
         }
     }
 }
