391ef2c4bfd6026d97f9cab49d07f92c7761f3eb
andrebeelders <andrebeelders@outlook.com>
2020-12-16 19:41:35 -0500

    intial commit of MersenneTwister

---
+++ src/Qtfy.Net.Numerics/Random/IRandomBitGenerator.cs
@@ -0,0 +1,24 @@
+// <copyright file="IRandomBitGenerator.cs" company="QuantifEye">
+// Copyright (c) QuantifEye. All rights reserved.
+// Licensed under the Apache 2.0 license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace Qtfy.Net.Numerics.Random
+{
+    /// <summary>
+    /// An interface for objects able to generate integral values that have random bits.
+    /// </summary>
+    /// <typeparam name="TBits">
+    /// The type of the value generated.
+    /// </typeparam>
+    public interface IRandomBitGenerator<TBits>
+    {
+        /// <summary>
+        /// Gets an integral value in the closed interval [T.Min, T.Max].
+        /// </summary>
+        /// <returns>
+        /// An integral value in the closed interval [T.Min, T.Max].
+        /// </returns>
+        TBits Next();
+    }
+}
