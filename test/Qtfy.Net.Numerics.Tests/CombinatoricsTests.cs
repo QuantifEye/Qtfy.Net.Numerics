3f21835d7d434a7cbbc85484f97bc53766429617
andrebeelders <andrebeelders@outlook.com>
2020-12-19 16:41:07 -0500

    fixed file header

--- test/Qtfy.Net.Numerics.Tests/CombinatoricsTests.cs
+++ test/Qtfy.Net.Numerics.Tests/CombinatoricsTests.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="CombinatoricsTests.PowerSet.cs" company="QuantifEye">
+﻿// <copyright file="CombinatoricsTests.cs" company="QuantifEye">
 // Copyright (c) QuantifEye. All rights reserved.
 // Licensed under the Apache 2.0 license.
 // See LICENSE.txt file in the project root for full license information.
